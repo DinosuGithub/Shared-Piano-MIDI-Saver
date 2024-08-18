235838e805b1ee13a0236939727055b6d8e0cc2c
Gideon Elliott <gideon.joseph.elliott@gmail.com>
2024-08-18 16:21:43 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,123 @@
+from mido import MidiFile, tempo2bpm, tick2second, second2tick
+import saving
+
+use_velocities = False
+START_DELAY = 10
+
+midi_path = input('MIDI file path: ')
+mid = MidiFile(midi_path, clip=True)
+result_instruments = []
+
+# Create song object to add notes to
+s = saving.Song()
+
+# Show instrument choices
+instruments = list(s.user_map.values())
+print('\nInstrument choices:')
+for instrument_i, instrument in enumerate(instruments):
+	print(f'{instrument_i + 1}: {instrument}')
+print()
+
+# Get the Shared Piano instrument substitutes for each track
+for track_i, track in enumerate(mid.tracks):
+  track_name = None
+  for message in track:
+    if message.type == 'track_name':
+      result_instrument = None
+
+      track_name = message.name
+      break
+
+  substitute_i = int(input(f'Instrument for track #{track_i} ("{track_name if track_name is not None else ""}"): ')) - 1
+  result_instruments.append(instruments[substitute_i])
+
+# Ask if velocities should be used
+use_velocities = input('\nUse note velocities? (y/n): ').lower() == 'y'
+
+# Initialize variables to be updated during processing
+notes = [] # To be saved in JSON format
+tracks = mid.tracks
+tempos = {0: 500000} # Tempos by tick number of tempo change
+
+# Get tempos
+for track_i, track in enumerate(tracks):
+  ticks_elapsed = 0
+
+  for message in track:
+    ticks_elapsed += message.time
+
+    if message.type == 'set_tempo':
+      tempos[ticks_elapsed] = message.tempo
+
+tempos = dict(sorted(tempos.items(), key=lambda item: item[0])) # Sort tempos
+
+# Update the notes list for each track
+print()
+for track_i, track in enumerate(tracks):
+  result_instrument = None
+  if track_i < len(result_instruments):
+    result_instrument = result_instruments[track_i]
+  track_icon = s.user_for_instrument(result_instrument)
+
+  ticks_elapsed = 0
+  track_notes = []
+  pending_notes = {}
+
+  tempo = tempos[0]
+  milestone_seconds_elapsed = 0
+  milestone_seconds_elapsed_tick = 0
+  milestone_ind = 0
+
+  for message in track:
+    ticks_elapsed += message.time
+
+    # Update tempo if tick has passed a tempo change tick
+    while True:
+      if milestone_ind + 1 < len(tempos.keys()):
+        next_tempo_tick = list(tempos.keys())[milestone_ind + 1]
+        if ticks_elapsed >= next_tempo_tick:
+          ticks_since_milestone = next_tempo_tick - milestone_seconds_elapsed_tick
+          milestone_seconds_elapsed += tick2second(ticks_since_milestone, mid.ticks_per_beat, tempo)
+          milestone_seconds_elapsed_tick = next_tempo_tick
+          tempo = tempos[next_tempo_tick]
+          milestone_ind += 1
+        else:
+          break
+      else:
+        break
+
+    ticks_since_milestone = ticks_elapsed - milestone_seconds_elapsed_tick
+    seconds_elapsed = milestone_seconds_elapsed + tick2second(ticks_since_milestone, mid.ticks_per_beat, tempo)
+
+	# For note_on messages, we do not know the end time of the note.
+	# We need to add it to a list of pending notes to be added when we get the note_off message.
+    if message.type == 'note_on':
+      pending_notes[message.note] = {
+        'start_seconds': seconds_elapsed,
+        'velocity': message.velocity if use_velocities else 50,
+      }
+    # Now we can add the note
+    elif message.type == 'note_off':
+      note_info = pending_notes[message.note]
+      track_notes.append({
+        'color': '#ff0000',
+        'midi': message.note,
+        'velocity': note_info['velocity'] / 128,
+        'pitch': 0,
+        'octave': 5,
+        'start': note_info['start_seconds'] * 60 + START_DELAY,
+        'end': seconds_elapsed * 60 + START_DELAY,
+        'emoji': track_icon,
+        'sustained': False
+      })
+
+  notes += track_notes
+
+  print(f'Done adding notes for track #{track_i + 1}.')
+
+# Save the song
+s.set_notes(notes)
+response = s.save()
+
+# Show the link to the saved song
+print(f'\nSaved link: https://musiclab.chromeexperiments.com/Shared-Piano/saved/#{response["name"]}')
---
+++ saving.py
@@ -0,0 +1,91 @@
+import requests
+import random
+import json
+
+class Song:
+  def __init__(self):
+    self.notes = []
+    self.user_map = {
+      '🐨': 'piano',
+      '🐮': 'strings',
+      '🐱': 'marimba',
+      '🐻': 'woodwind',
+      '🦊': 'synth',
+      '🐶': 'drum-kit',
+      '🐸': 'drum-machine',
+    }
+
+  def set_notes(self, notes):
+    self.notes = notes
+
+  def user_for_instrument(self, instrument):
+    if instrument in self.user_map.values():
+      return list(self.user_map.keys())[list(self.user_map.values()).index(instrument)]
+    else:
+      return self.user_for_instrument('piano')
+
+  def _generate_save_name(self):
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join([random.choice(chars) for _ in range(20)])
+
+  def save(self):
+    name = self._generate_save_name()
+
+    headers = {
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.9',
+      'content-type': 'multipart/related; boundary=64189289727574746616889059869415',
+      'origin': 'https://musiclab.chromeexperiments.com',
+      'priority': 'u=1, i',
+      'referer': 'https://musiclab.chromeexperiments.com/',
+      'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+      'x-firebase-storage-version': 'webjs/7.14.3',
+      'x-goog-upload-protocol': 'multipart',
+    }
+
+    params = {
+      'name': name,
+    }
+
+    song_data = {
+      "notes": self.notes,
+      "elapsed": max([note['end'] for note in self.notes]) + 10,
+      "users": self.user_map,
+      "keymap": {
+        "white": {
+          "0": {"note": "C", "pitch": "", "hidden": False},
+          "2": {"note": "D", "pitch": "", "hidden": False},
+          "4": {"note": "E", "pitch": "", "hidden": False},
+          "5": {"note": "F", "pitch": "", "hidden": False},
+          "7": {"note": "G", "pitch": "", "hidden": False},
+          "9": {"note": "A", "pitch": "", "hidden": False},
+          "11": {"note": "B", "pitch": "", "hidden": False}
+        },
+        "black": {
+          "1": {"note": "C", "pitch": "♯", "hidden": False},
+          "3": {"note": "D", "pitch": "♯", "hidden": False},
+          "6": {"note": "F", "pitch": "♯", "hidden": False},
+          "8": {"note": "G", "pitch": "♯", "hidden": False},
+          "10": {"note": "A", "pitch": "♯", "hidden": False}
+        }
+      },
+      "showNoteNames": False,
+      "showEmojis": True
+    }
+
+    data = (f'--64189289727574746616889059869415\r\nContent-Type: application/json; charset=utf-8\r\n\r\n{{"name":"{name}","contentType":"application/json"}}\r\n--64189289727574746616889059869415\r\nContent-Type: application/json\r\n\r\n' + json.dumps(song_data) + '\r\n--64189289727574746616889059869415--').encode()
+
+    response = requests.post(
+      'https://firebasestorage.googleapis.com/v0/b/cl-sharedpiano.appspot.com/o',
+      params=params,
+      headers=headers,
+      data=data,
+    )
+
+    return response.json()
